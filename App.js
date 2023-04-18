3cb879823498366b2f2d195c3dd3efbba4ddacdb
lunaray-hell <99817753+lunaray-hell@users.noreply.github.com>
2023-04-18 15:22:28 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,89 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import { Calculator } from "./apps/calculator";
+
+
+function hello(user){
+
+  if (user){
+    return <div>
+           <span>{user.name}</span>
+           <strong>{user.last_name}</strong>
+         </div>;
+  }
+  return 'undefined';
+}
+
+/*function это компонент*/ 
+
+function App() {
+
+  const a = 10;
+  const b = 15;
+  
+  let user = {
+       name: 'John',
+       last_name: 'Barlowe',
+       age: 30,
+       cat: {
+           name: 'Happy',
+           age: 1,
+       },
+       salaries: [
+    {
+        salary: '5670',
+        from_date: '2009'
+        },
+        {
+        salary: '1670',
+        from_date: '2018'
+        },
+        {
+        salary: '15670',
+        from_date: '2012'
+        },
+    ],
+    };
+  const salaries = user.salaries.map((salary, index) =>{
+    return <tr><td>{index + 1}</td><td>{salary.salary}</td><td>{salary.from_date}</td></tr> 
+  });
+
+// каждый html тег это компонент
+
+
+  const c = <span>Hello</span>;
+  const name = hello();
+
+/*выводим значение переменной    A: {a} */
+  return (
+    <div>
+     <p> A: {a} </p>  
+     <p> B: {b} </p>  
+
+     <p> Sum: {a + b} </p>  
+
+     <p>{c}, {name}</p>  
+
+     <Calculator/> 
+     <Calculator/> 
+     <Calculator/> 
+
+     <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Salary</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+        <tbody>
+          {salaries}
+        </tbody>
+     </table>
+    
+    </div>
+  );
+}
+
+export default App;
